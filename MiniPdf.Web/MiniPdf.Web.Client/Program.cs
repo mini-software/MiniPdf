74fc8f5c204b9f1b32ed9ffb2e3542d8a70c51ee
Wei Lin <shps951002@gmail.com>
2026-09-04 14:10:06 +0800

    fix: register Blazor root component (#126)
    Restore the App root component registration so the WebAssembly converter mounts into #app instead of remaining on the static Loading screen.

--- MiniPdf.Web/MiniPdf.Web.Client/Program.cs
+++ MiniPdf.Web/MiniPdf.Web.Client/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.JSInterop;
 using MiniPdf.Web.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddSingleton<I18n>();
